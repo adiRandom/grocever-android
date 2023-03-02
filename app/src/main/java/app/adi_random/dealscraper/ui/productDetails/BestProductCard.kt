137702728eb47ac29f63ab0fb60564036ee6dc1f
adi_random <me.adi.hf@gmail.com>
2023-03-02 16:54:35 +0200

    Add product image in product details

--- app/src/main/java/app/adi_random/dealscraper/ui/productDetails/BestProductCard.kt
+++ app/src/main/java/app/adi_random/dealscraper/ui/productDetails/BestProductCard.kt
@@ -8,27 +8,41 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import app.adi_random.dealscraper.data.models.ProductModel
+import app.adi_random.dealscraper.ui.text.HyperlinkText
 import app.adi_random.dealscraper.ui.theme.Colors
+import coil.compose.AsyncImage
 
 @Composable
 fun BestProductCard(product: ProductModel) {
-    Column(
+    Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(0.dp, 16.dp)
             .background(color = Colors.Background, shape = RoundedCornerShape(8.dp))
             .padding(16.dp, 8.dp)
     ) {
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(0.dp, 0.dp, 0.dp, 4.dp)
+        AsyncImage(
+            model = product.imageUrl,
+            contentDescription = "product image",
+            modifier = Modifier.size(96.dp)
+        )
+        Column(
+            modifier = Modifier.padding(horizontal = 8.dp),
         ) {
             Text(text = product.name, color = Colors.Secondary)
-            // TODO: Add Url
-            Text(text = product.bestStore.name, color = Colors.TextPrimary)
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 24.dp, 0.dp, 4.dp)
+            ) {
+                Text(
+                    text = "${product.bestPrice} RON / ${product.purchaseInstalments[0].unitName}",
+                    color = Colors.TextPrimary
+                )
+                HyperlinkText(text = product.bestStore.name, link = product.url)
+            }
         }
-        Text(text = "${product.bestPrice} RON / ${product.purchaseInstalments[0].unitName}", color = Colors.TextPrimary)
     }
 }
